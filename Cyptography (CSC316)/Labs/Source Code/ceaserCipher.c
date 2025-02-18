3c5f5ff41f5d01d569b8cbf9fb245dadde585e09
codewotharjun <dolkhaliarjun9800@gmail.com>
2025-02-18 13:46:59 +0545

    sample lab report

---
+++ Cyptography (CSC316)/Labs/Source Code/ceaserCipher.c
@@ -0,0 +1,119 @@
+// Encryption of a message
+
+#include<stdio.h>
+
+#include<ctype.h>
+
+int main() {
+
+    char text[500], ch;
+
+    int key;
+
+    // Taking user input.
+    printf("Enter a message to encrypt: ");
+
+    scanf("%s", text);
+
+    printf("Enter the key: ");
+
+    scanf("%d", & key);
+
+    // Visiting character by character.
+
+    for (int i = 0; text[i] != '\0'; ++i) {
+
+        ch = text[i];
+        // Check for valid characters.
+        if (isalnum(ch)) {
+
+            //Lowercase characters.
+            if (islower(ch)) {
+                ch = (ch - 'a' + key) % 26 + 'a';
+            }
+            // Uppercase characters.
+            if (isupper(ch)) {
+                ch = (ch - 'A' + key) % 26 + 'A';
+            }
+
+            // Numbers.
+            if (isdigit(ch)) {
+                ch = (ch - '0' + key) % 10 + '0';
+            }
+        }
+        // Invalid character.
+        else {
+            printf("Invalid Message");
+        }
+
+        // Adding encoded answer.
+        text[i] = ch;
+
+    }
+
+    printf("Encrypted message: %s", text);
+
+    return 0;
+}
+
+
+
+
+
+//Decryption 
+
+/*
+#include<stdio.h>
+#include<ctype.h>
+
+int main() {
+
+    char text[500], ch;
+
+    int key;
+
+    // Taking user input.
+
+    printf("Enter a message to decrypt: ");
+
+    scanf("%s", text);
+
+    printf("Enter the key: ");
+
+    scanf("%d", & key);
+
+    // Visiting each character.
+    for (int i = 0; text[i] != '\0'; ++i) {
+
+        ch = text[i];
+        // Check for valid characters.
+        if (isalnum(ch)) {
+            //Lowercase characters.
+            if (islower(ch)) {
+                ch = (ch - 'a' - key + 26) % 26 + 'a';
+            }
+            // Uppercase characters.
+            if (isupper(ch)) {
+                ch = (ch - 'A' - key + 26) % 26 + 'A';
+            }
+            // Numbers.
+            if (isdigit(ch)) {
+                ch = (ch - '0' - key + 10) % 10 + '0';
+            }
+        }
+        // Invalid characters.
+        else {
+            printf("Invalid Message");
+        }
+        // Adding decoded character back.
+        text[i] = ch;
+
+    }
+
+    printf("Decrypted message: %s", text);
+
+    return 0;
+
+}
+
+*/
